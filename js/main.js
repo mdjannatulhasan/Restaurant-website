1c9286955109f9555329c948dd1a3b1fca125dc3
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-06-09 23:06:39 +0600

    menu dropdown

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$('.navbar-toggler').click(function(){
-    $('.small-header').toggleClass('height-expand');
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+        $('.small-header').toggleClass('height-expand');
+    });
+    $(".dining-menu .dropdown-menu .nav-link").click(function(){
+        var x = $(this).text();
+        document.getElementById("dropdown-name").innerHTML = x;
+    });
+    
 });
