6d1e8e05df493d4ded5a3acb87200e74bacffbc7
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-06-10 13:01:33 +0600

    n

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 $(document).ready(function(){
-    $('.navbar-toggler').click(function(){
-        $('.small-header').toggleClass('height-expand');
-    });
+    // $('.navbar-toggler').click(function(){
+    //     $('header .small-header').toggleClass('height-expand');
+    //     console.log("hhhhh");
+    // });
     $(".dining-menu .dropdown-menu .nav-link").click(function(){
         var x = $(this).text();
         document.getElementById("dropdown-name").innerHTML = x;
     });
     
-    $(window).on('load',function(){
-        $(".owl-carousel-2").owlCarousel({
-            loop: true,
-            margin:30,
-            dots:true,
-            autoplay: true,
-            autoplayTimeout: 3000,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1
-                },
-                576:{
-                    items:1
-                },
-                768:{
-                    items:2
-                },
-                992:{
-                    items:3,
-                    autoplay:false
-                }
+    $(".owl-carousel-2").owlCarousel({
+        loop: true,
+        margin:30,
+        dots:true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3,
+                autoplay:false
             }
-        });
-    })
+        }
+    });
 });
